d965510803ec9cfe09e8518724b3dea3dc1486be
Eli <elikaykova@gmail.com>
2018-11-05 12:32:24 +0200

    New identation

--- pathSearch.js
+++ pathSearch.js
@@ -1,44 +1,46 @@
-let data1 = [{width: 3000, height:3000},
-			{width: 3000, height:4000},
-			{width: 3000, height:4000},
-			{width: 3000, height:3000},
-			{width: 3000, height:4000},
-			{width: 4000, height:3000},
-			{width: 4000, height:3000},
-			{width:  224, height: 224},
-			{width:  780, height: 585},
-			{width: 1000, height:1500},
-			{width: 1920, height:1200},
-			{width: 1280, height: 600}
-		   ];
+/* eslint-disable no-console */
 
-let data2 = [{width: 3000, height:3000},
-			 {width: 3000, height:3000},
-			 {width: 4000, height:3000},
-			 {width: 4000, height:3000},
-			 {width:  224, height: 224},
-			 {width:  780, height: 585},
-			 {width: 1000, height:1500},
-			 {width: 1920, height:1200},
-			 {width: 1280, height: 600}
-		   ];
+let data1 = [
+    { width: 3000, height: 3000 },
+    { width: 3000, height: 4000 },
+    { width: 3000, height: 4000 },
+    { width: 3000, height: 3000 },
+    { width: 3000, height: 4000 },
+    { width: 4000, height: 3000 },
+    { width: 4000, height: 3000 },
+    { width: 224, height: 224 },
+    { width: 780, height: 585 },
+    { width: 1000, height: 1500 },
+    { width: 1920, height: 1200 },
+    { width: 1280, height: 600 }
+];
+
+let data2 = [
+    { width: 3000, height: 3000 },
+    { width: 3000, height: 3000 },
+    { width: 4000, height: 3000 },
+    { width: 4000, height: 3000 },
+    { width: 224, height: 224 },
+    { width: 780, height: 585 },
+    { width: 1000, height: 1500 },
+    { width: 1920, height: 1200 },
+    { width: 1280, height: 600 }
+];
 
 const data = data2;
 const viewport = 1440;
 const idealRowHeight = 300;
-const maxShrinkFactor = 100;
-const maxStretchFactor = 100;
+const maxShrinkFactor = 50;
+const maxStretchFactor = 50;
 const photoNumber = 9;
 
 const START = 0;
 let nodes = [];
-let PATH = []; 
+let PATH = [];
 let result = [];
 let res;
 
-
 function Node(parent, value, weight) {
-
     this.value = value;
     this.children = [];
     this.parent = parent;
@@ -46,118 +48,139 @@ function Node(parent, value, weight) {
     this.badness = 0;
 
     this.setBadness = function() {
-    	this.badness = this.weight * this.weight + this.parent.badness; 
-    }
+        this.badness = this.weight * this.weight + this.parent.badness;
+    };
 
     this.getBadness = function() {
         return this.badness;
-    }
+    };
 
     this.setParentNode = function(node) {
         this.parent = node;
-    }
+    };
 
     this.getParentNode = function() {
         return this.parent;
-    }
+    };
 
     this.addChild = function(node, weight) {
         node.setParentNode(this);
         this.children[this.children.length] = node;
         node.setWeight(weight);
-    }
+    };
 
     this.getChildren = function() {
         return this.children;
-    }
+    };
 
     this.setWeight = function(weight) {
-    	this.weight = weight;
-    	this.setBadness();
-    }
+        this.weight = weight;
+        this.setBadness();
+    };
 
     this.getWeight = function() {
         return this.weight;
-    }
+    };
 }
 
-
 function findBestHeight(accumulatedWidth) {
-	return Math.floor(viewport*idealRowHeight/accumulatedWidth);
+    return Math.floor((viewport * idealRowHeight) / accumulatedWidth);
 }
 
 function scalingUpToFit(height, currHeight, currWidth) {
-	return height*currWidth/currHeight;
+    return (height * currWidth) / currHeight;
 }
 
 function scalingDownToFit(height, currHeight, currWidth) {
-	return height*currWidth/currHeight;
+    return (height * currWidth) / currHeight;
 }
 
 function resize(start, end, height) {
-	let widthSum = 0;
-	for(var i = start; i < end - 1; i++) {
-		currWidth = Math.floor(scalingDownToFit(height, data[i].height, data[i].width));
-		result.push({"width": currWidth, "height": height});
-		widthSum += currWidth;
-	}
-	result.push({"width": viewport - widthSum, "height": height});
+    let widthSum = 0;
+    for (var i = start; i < end - 1; i++) {
+        let currWidth = Math.floor(
+            scalingDownToFit(height, data[i].height, data[i].width)
+        );
+        result.push({ width: currWidth, height: height });
+        widthSum += currWidth;
+    }
+    result.push({ width: viewport - widthSum, height: height });
 }
 
 function goTroughtGroups(start, end) {
-	let currWidth = 0;
-	for(var i = start; i < end; i++) {
-		currWidth += Math.floor(scalingDownToFit(idealRowHeight, data[i].height, data[i].width));
-		//console.log("photo(" + i + ") : ");
-		//console.log(idealRowHeight + " " + data[i].height + " " + data[i].width);
-	}
+    let currWidth = 0;
+    for (var i = start; i < end; i++) {
+        currWidth += Math.floor(
+            scalingDownToFit(idealRowHeight, data[i].height, data[i].width)
+        );
+        //console.log("photo(" + i + ") : ");
+        //console.log(idealRowHeight + " " + data[i].height + " " + data[i].width);
+    }
 
-	let restWidth = viewport - currWidth;
-	let currHeight = findBestHeight(currWidth);
-	console.log("currHeight: " + currHeight);
-	resize(start, end, currHeight);
+    let restWidth = viewport - currWidth;
+    let currHeight = findBestHeight(currWidth);
+    console.log("currHeight: " + currHeight);
+    resize(start, end, currHeight);
 }
 
 function goTroughtBreaks() {
-	//console.log(PATH.length);
-	for(var i = 1; i < PATH.length; i++) {
-		goTroughtGroups(PATH[i-1], PATH[i]);
-		//console.log("hey");
-	}
+    //console.log(PATH.length);
+    for (var i = 1; i < PATH.length; i++) {
+        goTroughtGroups(PATH[i - 1], PATH[i]);
+        //console.log("hey");
+    }
 }
 
 function findWeight(start, end) {
-	let currWidth = 0;
-	for(var i = start; i < end; i++) {
-		currWidth += Math.floor(scalingDownToFit(idealRowHeight, data[i].height, data[i].width));
-	}
+    let currWidth = 0;
+    for (var i = start; i <= end; i++) {
+        currWidth += Math.floor(
+            scalingDownToFit(idealRowHeight, data[i].height, data[i].width)
+        );
+    }
 
-	//console.log(currWidth);
-	//console.log(findBestHeight(currWidth));
-	return findBestHeight(currWidth);
+    let currHeight = findBestHeight(currWidth);
+    let badness = Math.abs(idealRowHeight - currHeight);
+    console.log("weight from " + start + " to " + end + ": " + badness);
+    if (Math.abs(idealRowHeight - currHeight) > maxShrinkFactor) {
+        return badness * badness + (viewport - currWidth);
+    }
+    if (currWidth < viewport) {
+        return badness + (viewport - currWidth);
+    }
+
+    console.log("currWidth: badness" + currWidth);
+    console.log(findBestHeight(currWidth));
+    return badness;
 }
 
 function exists(node) {
-	for(var i = 0; i < nodes.length; i++) {
-		if(nodes[i].value == node) return i;
-		return false;
-	}
+    for (var i = 0; i < nodes.length; i++) {
+        if (nodes[i].value == node) return i;
+        return false;
+    }
 }
 
 function findChildren(currIndex) {
-	console.log("nodes_length: " + nodes.length);
-	console.log("parent " + currIndex);
-	let widthSum = 0;
-	while(widthSum < viewport && currIndex < photoNumber) {
-		widthSum += Math.floor(scalingDownToFit(idealRowHeight, data[currIndex].height, data[currIndex].width));
-		//console.log("widthSum: " + widthSum);
-		currIndex++;
-	}
-	console.log("child_1 " + (currIndex - 2));
-	if(currIndex >= photoNumber) return currIndex - 1;
-	currIndex--; // loop is one step longer
-	console.log("child_2 " + (currIndex));
-	return currIndex - 1;
+    console.log("nodes_length: " + nodes.length);
+    console.log("parent " + currIndex);
+    let widthSum = 0;
+    while (widthSum < viewport && currIndex < photoNumber) {
+        widthSum += Math.floor(
+            scalingDownToFit(
+                idealRowHeight,
+                data[currIndex].height,
+                data[currIndex].width
+            )
+        );
+        //console.log("widthSum: " + widthSum);
+        currIndex++;
+    }
+    console.log("child_1 " + (currIndex - 2));
+    if (currIndex >= photoNumber) return currIndex - 1;
+    currIndex--; // loop is one step longer
+    console.log("child_2 " + currIndex);
+    return currIndex - 1;
 }
 
 function generateGraph() {
@@ -166,57 +189,88 @@ function generateGraph() {
     nodes.push(new Node(null, START, 0));
     let currIndex = 0;
 
-    while(notFound || notDouble) {
-    	console.log("times left from first found path: " + notDouble);
+    while (notFound || notDouble) {
+        console.log("times left from first found path: " + notDouble);
         let children = [];
         children[0] = findChildren(nodes[currIndex].value);
         children[1] = children[0] + 1;
 
-        for(var i = 0; i < 2; i++) {
-        	if(children[i] >= photoNumber) {
-        		notDouble--;
-        		notFound = false;
+        for (var i = 0; i < 2; i++) {
+            if (children[i] >= photoNumber) {
+                notDouble--;
+                notFound = false;
 
-        		if(res)console.log("ResBadness: " + res.getBadness() + " currBadness: " + nodes[currIndex].getBadness());
-        		console.log("resB: " + res);
-        		if(!res || res.getBadness() > nodes[currIndex].getBadness())
-        			res = nodes[currIndex];
-        		console.log("resA: " + res.value);
-        		if(i == 1) break;
-        	}
+                if (res)
+                    console.log(
+                        "ResBadness: " +
+                            res.getBadness() +
+                            " currBadness: " +
+                            nodes[currIndex].getBadness()
+                    );
+                if (res) console.log("resB: " + res.value);
+                if (!res || res.getBadness() > nodes[currIndex].getBadness())
+                    res = nodes[currIndex];
+                console.log("resA: " + res.value);
+                if (i == 1) break;
+            }
 
-        	let position = exists(children[i]);
-        	let currNode = new Node(nodes[currIndex], children[i], idealRowHeight - findWeight(nodes[currIndex].value, children[i]));
-        	console.log("new node: " + nodes[currIndex].value + " " + children[i]);
-        	currNode.setBadness();
-        	if(!position) nodes.push(currNode);
-        	else if(currNode.getBadness() < nodes[position].getBadness()) { 
-        		console.log(nodes.position + " -> " + children[i]);
-        		nodes[position] = currNode;
-        	}	
+            let position = exists(children[i]);
+            let currNode = new Node(
+                nodes[currIndex],
+                children[i],
+                nodes[currIndex].getWeight() +
+                    findWeight(nodes[currIndex].value, children[i])
+            );
+            console.log(
+                "new node: " +
+                    nodes[currIndex].value +
+                    " " +
+                    children[i] +
+                    " weight: " +
+                    nodes[currIndex].getWeight()
+            );
+            currNode.setBadness();
+            if (!position) nodes.push(currNode);
+            else if (currNode.getBadness() < nodes[position].getBadness()) {
+                console.log(nodes.position + " -> " + children[i]);
+                nodes[position] = currNode;
+            }
         }
-    	currIndex++;
+        currIndex++;
     }
 }
 
-function findPath(node){
-	if(node.value == START) {
-		//console.log(node.value);
-		PATH.push(node.value);
-		return;
-	}
+function findPath(node) {
+    if (node.value == START) {
+        //console.log(node.value);
+        PATH.push(node.value);
+        return;
+    }
 
-	findPath(node.getParentNode());
-	//console.log(node.value);
-	PATH.push(node.value);
+    findPath(node.getParentNode());
+    //console.log(node.value);
+    PATH.push(node.value);
 }
 
 function test() {
-	generateGraph();
-	console.log("result: " + res.value);
-	findPath(res);
-	PATH.push(photoNumber);
-	console.log(PATH);
+    generateGraph();
+    console.log("result: " + res.value);
+    findPath(res);
+    PATH.push(photoNumber);
+    console.log(PATH);
+    goTroughtBreaks();
+    console.log(result);
+
+    for (var i = 1; i < nodes.length; i++)
+        console.log(
+            i +
+                " " +
+                nodes[i].value +
+                " " +
+                nodes[i].parent.value +
+                " " +
+                nodes[i].badness
+        );
 }
 
 test();
